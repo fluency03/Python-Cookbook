1331a2639e15360c45da76f9a5448feec3583a6f
fluency03 <fluency.03@gmail.com>
2015-04-18 22:25:45 +0200

    1.15

---
+++ 1.16.filter_seq_elements.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+# 1.16. Filtering Sequence Elements
+# Problem: You have data inside of a sequence, and need to extract values or reduce the sequence
+# using some criteria.
+
+
+
+
