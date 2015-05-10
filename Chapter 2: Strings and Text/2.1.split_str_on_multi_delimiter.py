040ab1ef41e107798de30621c45876ac77eee10d
cliu <fluency.03@gmail.com>
2015-05-10 22:14:47 +0200

    2.1.split_str_on_multi_delimiter.py

--- Chapter 2: Strings and Text/2.1.split_str_on_multi_delimiter.py
+++ Chapter 2: Strings and Text/2.1.split_str_on_multi_delimiter.py
@@ -13,10 +13,27 @@ import re
 # r'[;,\s]\s*'   ; or , or ' ' plus a ' ' with amount of nature number
 split_line = re.split(r'[;,\s]\s*', line)
 
+# as shown in the solution, the separator is either a comma (,),semicolon (;), 
+# or whitespace followed by any amount of extra whitespace. 
 print (split_line)
 
+new_line = line.replace(',',' ').replace(';',' ').split()
+print (new_line)
+
+# --------------------------------------------------------------------------
+
+fields = re.split(r'(;|,|\s)\s*', line)
+print (fields) # ['asdf', ' ', 'fjdk', ';', 'afed', ',', 'fjek', ',', 'asdf', ',', 'foo']
+
+values = fields[0:-1:2]
+delimiters = fields[1::2] + ['']
+
+print (values) # ['asdf', 'fjdk', 'afed', 'fjek', 'asdf', 'foo']
+print (delimiters) # [' ', ';', ',', ',', ',', '']
 
 
+joined = ''.join(v+d for v,d in zip(values, delimiters))
+print (joined)
 
-
-
+another_line = re.split(r'(?:,|;|\s)\s*', line)
+print (another_line)
