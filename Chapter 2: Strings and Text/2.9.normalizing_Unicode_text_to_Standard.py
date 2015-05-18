c0ba1b58a9e85a37e95d129f4f5a7365d090659b
cliu <fluency.03@gmail.com>
2015-05-18 15:03:03 +0200

    .

---
+++ Chapter 2: Strings and Text/2.9.normalizing_Unicode_text_to_Standard.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# 2.9. Normalizing Unicode Text to a Standard Representation
+# Problem
+# You’re working with Unicode strings, but need to make sure that all of the strings have
+# the same underlying representation.
+
+
+
+
