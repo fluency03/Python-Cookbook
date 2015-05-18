714f2abf686576c3cff0e9671558d4edb545c7e5
cliu <fluency.03@gmail.com>
2015-05-18 15:44:26 +0200

    2.10

---
+++ Chapter 2: Strings and Text/2.10.working_with_Unicode_Characters_in_regex.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# 2.10. Working with Unicode Characters in Regular Expressions
+# Problem
+# You are using regular expressions to process text, but are concerned about the handling
+# of Unicode characters.
+
+import re
+
+
+
+
+
+
+
+
+
+
+
