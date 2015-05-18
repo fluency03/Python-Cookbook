27d88b3e4bddbea8cb111da198660fd5ecdf772c
cliu <fluency.03@gmail.com>
2015-05-18 15:44:12 +0200

    2.9

--- Chapter 2: Strings and Text/2.9.normalizing_Unicode_text_to_Standard.py
+++ Chapter 2: Strings and Text/2.9.normalizing_Unicode_text_to_Standard.py
@@ -4,6 +4,19 @@
 # You’re working with Unicode strings, but need to make sure that all of the strings have
 # the same underlying representation.
 
+s1 = 'Spicy Jalape\u00f1o'
+s2 = 'Spicy Jalapen\u0303o'
+print (s1, s2)
+print (ascii(s1), ascii(s2))
 
+import unicodedata
 
+t1 = unicodedata.normalize('NFC', s1)
+t2 = unicodedata.normalize('NFC', s2)
+print (t1, t2)
+print (ascii(t1), ascii(t2))
 
+t3 = unicodedata.normalize('NFD', s1)
+t4 = unicodedata.normalize('NFD', s2)
+print (t3, t4)
+print (ascii(t3), ascii(t4))
