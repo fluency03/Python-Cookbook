f1983436a79bb13b711df2b52f83aa2c89fdc88b
cliu <fluency.03@gmail.com>
2015-07-29 16:41:05 +0200

    2.17

--- Chapter 2: Strings and Text/2.17.handling_HTML_and_XML_entities_in_text.py
+++ Chapter 2: Strings and Text/2.17.handling_HTML_and_XML_entities_in_text.py
@@ -5,22 +5,35 @@
 # corresponding text. Alternatively, you need to produce text, but escape certain charac‐
 # ters (e.g., < , > , or & ).
 
+import html
+
+s = 'Elements are written as "<tag>text</tag>".'
+print (s)
+print (html.escape(s))
+
+# Disable escaping of quotes
+print (html.escape(s, quote=False))
+
+# -------------------------------------------------------------------
+# emit text as ASCII and want to embed character code entities for 
+# non-ASCII characters, you can use the errors='xmlcharrefreplace'
+d = 'Spicy Jalapeño'
+dn = d.encode('ascii', errors='xmlcharrefreplace')
+print (dn)
+
+# -------------------------------------------------------------------
+from html.parser import HTMLParser
+t = 'Spicy &quot;Jalape&#241;o&quot.'
+p = HTMLParser()
+print (p.unescape(t))
 
 
+n = 'The prompt is &gt;&gt;&gt;'
+from xml.sax.saxutils import unescape
+print (unescape(n))
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+# If you’re actually processing HTML or XML, try using a proper HTML or XML parser first.
 
 
 
